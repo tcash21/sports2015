2e6c522e9c21cdf17221a4ecf6fc918e4655c3ff
EC2 Default User <ec2-user@ip-10-111-168-213.ec2.internal>
2015-10-29 12:23:12 -0400

    first commit

---
+++ NBA/scrapeNBAteams.py
@@ -0,0 +1,77 @@
+__author__ = 'tanyacashorali'
+
+import random
+import urllib2
+import time
+import re
+import random
+from datetime import date, timedelta
+import os
+import sqlite3
+import pandas as pd
+from urlparse import urlparse
+from bs4 import BeautifulSoup as bs
+
+db = sqlite3.connect('/home/ec2-user/sports2015/NBA/sports.db')
+
+
+x=random.randint(1, 20)
+time.sleep(x)
+
+def index():
+    print "entered index"
+    times = []
+    halftime_ids = []
+    today = date.today() - timedelta(days=1)
+    today = today.strftime("%Y%m%d")
+#    today = '20151028'
+    url = urllib2.urlopen('http://scores.espn.go.com/nba/scoreboard?date=' + today)
+    soup = bs(url.read(), ['fast', 'lxml'])
+    #scoreboard=soup.findAll('div', {'id': 'scoreboard-page'})
+    #data=scoreboard[0].get('data-data')
+    teams = re.findall('http://espn.go.com/nba/team/_/name/(\w+)', str(soup))
+
+    for i in range(0, len(teams)):
+        x=random.randint(5, 10)
+        time.sleep(x)
+        print str(i) + ' out of ' + str(len(teams)) + ' teams.'
+        espn = 'http://espn.go.com/nba/team/stats/_/name/' + teams[i]
+        print espn
+        #id = re.search('=(\d+)', link_strings[i]).group(1)
+        url = urllib2.urlopen(espn)
+        soup1 = bs(url.read(), ['fast', 'lxml'])
+        
+        ## e.g. no season stats for http://espn.go.com/mens-college-basketball/team/stats/_/id/2395
+        try:
+            totals1 = soup1.find_all('tr', {'class':'total'})[1]
+            data1 = totals1.findAll('td')
+            values1 = [d.text for d in data1]
+            cols1 = soup1.findAll('tr', {'class':'colhead'})[1]
+            colnames1 = cols1.findAll('a')
+            colnames1 = [c.text for c in colnames1]
+
+            totals0 = soup1.find_all('tr', {'class':'total'})[0]
+            data0 = totals0.findAll('td')
+            values0 = [d.text for d in data0]
+            cols0 = soup1.findAll('tr', {'class':'colhead'})[0]
+            colnames0 = cols0.findAll('a')
+            colnames0 = [c.text for c in colnames0]
+
+            try:
+                with db:
+                    db.execute('''INSERT INTO NBAseasonstats(team, the_date, fgm, fga, fgp, tpm, tpa, tpp, ftm, fta, ftp, twopm, twopa, twopp, pps, afg) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)''', (teams[i].upper(),today.strftime("%m/%d/%Y"),float(values1[1]),float(values1[2]),float(values1[3]),float(values1[4]),float(values1[5]),float(values1[6]),float(values1[7]),float(values1[8]),float(values1[9]),float(values1[10]),float(values1[11]),float(values1[12]),float(values1[13]),float(values1[14])))
+                    db.commit()
+            except sqlite3.IntegrityError:
+                print 'Team1 Season Stats Already Exist!'
+            try: 
+                with db:
+                    db.execute('''INSERT INTO NBAseasontotals(team, the_date, gp, ppg, orpg, defr, rpg, apg, spg, bpg, tpg, fpg, ato) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?)''', (teams[i].upper(), today.strftime("%m/%d/%Y"), int(values0[1]),  float(values0[4]), float(values0[5]), float(values0[6]), float(values0[7]), float(values0[8]), float(values0[9]), float(values0[10]), float(values0[11]), float(values0[12]), float(values0[13])))
+                    db.commit()
+            except sqlite3.IntegrityError:
+                print 'Team1 Season Totals Already Exist!'
+        except:
+            print 'No stats for team1'
+
+index()
+db.close()
+
---
+++ NBA/scrapeSportsBookNBAHalfLines.py
@@ -0,0 +1,67 @@
+__author__ = 'tanyacashorali'
+
+import urllib2
+import time
+import re
+import random
+import datetime
+import os
+import sqlite3
+import pandas as pd
+from urlparse import urlparse
+from bs4 import BeautifulSoup as bs
+from datetime import date, timedelta
+
+db = sqlite3.connect('/home/ec2-user/sports2015/NBA/sports.db')
+
+x=random.randint(1, 20)
+time.sleep(x)
+
+url = urllib2.urlopen('https://www.sportsbook.ag/sbk/sportsbook4/nba-betting/nba-2nd-half-lines.sbk')
+#url = urllib2.urlopen('https://www.sportsbook.ag/sbk/sportsbook4/nba-playoffs-betting/nba-2nd-half-lines.sbk')
+soup = bs(url.read(), ['fast', 'lxml'])
+divs=soup.findAll('div', {'class':'col-sm-12 eventbox'})
+awayTeams=[d.findAll('span', {'id':'awayTeamName'}) for d in divs]
+homeTeams=[d.findAll('span', {'id':'homeTeamName'}) for d in divs]
+awayTeams=[a[0].text for a in awayTeams]
+homeTeams=[h[0].text for h in homeTeams]
+awayTeams=filter(len, awayTeams)
+homeTeams=filter(len, homeTeams)
+
+market=[d.findAll('div', {'class':'market'}) for d in divs]
+market=filter(len, market)
+the_lines=[m[0].text for m in market]
+the_spreads=[m[1].text for m in market]
+
+lines = []
+spreads = []
+
+for i in range(0, len(the_lines)):
+    try:
+        lines.append(re.search('(\d+\\.?\d+)\\(', the_lines[i]).group(1))
+        spreads.append(re.search('([+-]?\d+\\.?\d?)\\(', the_spreads[i]).group(1))
+    except:
+        next
+
+today = str(datetime.datetime.now() - timedelta(hours=2))[0:10]
+today = time.strftime("%m/%d/%Y", time.strptime(today, '%Y-%m-%d'))
+
+date_time = str(datetime.datetime.now())
+
+for i in range(0, len(lines)):
+    try:
+        with db:
+            db.execute('''INSERT INTO NBASBHalfLines(away_team, home_team, line, spread, game_date, game_time) VALUES(?,?,?,?,?,?)''', (awayTeams[i], homeTeams[i], lines[i], spreads[i], today, date_time))
+            db.commit()
+    except sqlite3.IntegrityError:
+        print 'Record Exists'
+
+for i in range(0, len(lines)):
+    try:
+        with db:
+            db.execute('''INSERT INTO NBASBteamlookup(sb_team, espn_abbr) VALUES (?,?)''', (awayTeams[i], None))
+            db.execute('''INSERT INTO NBASBteamlookup(sb_team, espn_abbr) VALUES (?,?)''', (homeTeams[i], None))
+    except:
+        print 'Record Exists'
+
+db.close()
